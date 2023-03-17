c5c0d88dffb653e36450f198b4f0ab6c0d6342ca
tracer-ics2017 <tracer@njuics.org>
2023-03-17 11:12:52 +0800

    >  compile 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  11:12:52 up  1:10,  1 user,  load average: 0.60, 0.47, 0.33 fa0cef18927ab6265b19805b8277a992a954ce5c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -112,7 +112,18 @@ static bool make_token(char *e) {
           case  TK_NOTYPE://空格
             break;
           case '+':
-            //printf("here \n");
+            strncpy(tokens[nr_token].str,substr_start,substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
+            break;
+          case '-':
+            strncpy(tokens[nr_token].str,substr_start,substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
+            break;
+          case '*':
+            strncpy(tokens[nr_token].str,substr_start,substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
+            break;
+          case '/':
             strncpy(tokens[nr_token].str,substr_start,substr_len);
             tokens[nr_token].str[substr_len] = '\0';
             break;
