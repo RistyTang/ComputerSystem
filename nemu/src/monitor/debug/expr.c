48dc72dacf03ba629407a4d7d618126e9c374c07
tracer-ics2017 <tracer@njuics.org>
2023-03-17 18:14:48 +0800

    >  compile 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  18:14:48 up  8:10,  1 user,  load average: 0.43, 0.16, 0.21 c137cba951cf5ae0fbd8d8a9fe29ae0e805b4fe7

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -29,8 +29,7 @@ static struct rule {
   {"==", TK_EQ},         // equal
   {"!=", TK_NEQ},        // not equal
   {"0|[1-9][0-9]*",TK_DECIMIAL}, //10
-  {"0x[0-9A-Fa-f]*|0X[0-9A-Fa-f]*",TK_HEX}, //hexadecimial
-  //{"[1-9][0-9]*|0",TK_DECIMIAL}, //10
+  {"0x[0-9A-Fa-f]*",TK_HEX}, //hexadecimial
   {"0[0-7]*",TK_OCTAL},  //8
   {"\\-", '-'},          // sub
   {"\\*", '*'},          // mul
