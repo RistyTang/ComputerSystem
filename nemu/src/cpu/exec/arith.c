bd94bd9d64cc3ee25600848e4cf8dcc8f8c4903b
tracer-ics2017 <tracer@njuics.org>
2023-04-13 23:04:04 +0800

    >  compile 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  23:04:04 up  2:05,  1 user,  load average: 0.66, 0.34, 0.16 1f0b6205a6afdbba97b5e433d78275adbbd14c20

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -21,9 +21,8 @@ make_EHelper(add) {
 
 make_EHelper(sub) {
   //TODO();
-  /*
-  rtl_sub(&t0,&id_dest->val,&id_src->val);//dest - src
-  operand_write(id_dest,&t0);
+  rtl_sub(&t2,&id_dest->val,&id_src->val);//dest - src
+  operand_write(id_dest,&t2);
   //eflags
   //ZFSF
   rtl_update_ZFSF(&t0,id_dest->width);
@@ -36,7 +35,7 @@ make_EHelper(sub) {
   rtl_and(&t0,&t1,&t2);
   rtl_msb(&t0,&t0,id_dest->width);
   rtl_set_OF(&t0);
-  */
+ /*
   rtl_sub(&t2,&id_dest->val,&id_src->val);//dest - src
   rtl_update_ZFSF(&t2,id_dest->width);
   rtl_sltu(&t1,&id_dest->val,&id_src->val);
@@ -48,6 +47,7 @@ make_EHelper(sub) {
   rtl_set_OF(&t0);
 
   operand_write(id_dest,&t2);
+  */
 
   print_asm_template2(sub);
 }
