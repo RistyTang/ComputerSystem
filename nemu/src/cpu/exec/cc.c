874cfe9dc79f851ea9396c5e486b0e083fe78407
tracer-ics2017 <tracer@njuics.org>
2023-04-13 21:13:56 +0800

    >  compile 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  21:13:56 up 15 min,  1 user,  load average: 0.46, 0.30, 0.19 ff9fd5e5282d5befef826d9c111282397f8b87c7

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -40,12 +40,11 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
       rtl_xor(dest,dest,&t0);//SF != OF
       break;
     case CC_LE://14
-      assert(dest!=&t0);
-      rtl_get_SF(dest);
-      rtl_get_OF(&t0);
-      rtl_xor(dest,dest,&t0);
       rtl_get_ZF(&t0);
-      rtl_or(dest,dest,&t0);//ZF=1 || SF!=OF
+      rtl_get_SF(&t1);
+      rtl_get_OF(&t2);
+      rtl_xor(&t3,&t1,&t2);//SF!=OF
+      rtl_or(dest,&t0,&t3);//ZF=1
       break;
     case CC_NE:
       rtl_get_ZF(&t0);
