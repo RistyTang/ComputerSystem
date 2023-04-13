9c3871ec3e79fd896f4f55e87b4910786e16a708
tracer-ics2017 <tracer@njuics.org>
2023-04-13 21:33:50 +0800

    >  compile 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  21:33:50 up 35 min,  1 user,  load average: 1.56, 0.85, 0.54 7e3c8b761a0aa58c16d495289057cc9999bb20f2

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -57,9 +57,9 @@ make_EHelper(sar) {
 make_EHelper(shl) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_shl(&t0,&id_dest->val,&id_src->val);
-  operand_write(id_dest,&t0);
-  rtl_update_ZFSF(&t0,id_dest->width);
+  rtl_shl(&t1,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t1);
+  rtl_update_ZFSF(&t1,id_dest->width);
 
   print_asm_template2(shl);
 }
@@ -67,9 +67,9 @@ make_EHelper(shl) {
 make_EHelper(shr) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_shr(&t0,&id_dest->val,&id_src->val);
-  operand_write(id_dest,&t0);
-  rtl_update_ZFSF(&t0,id_dest->width);
+  rtl_shr(&t1,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t1);
+  rtl_update_ZFSF(&t1,id_dest->width);
 
   print_asm_template2(shr);
 }
