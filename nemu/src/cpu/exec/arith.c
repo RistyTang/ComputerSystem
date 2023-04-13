d4301f80763d92cbc2f1296a3273a4d1d31f40db
tracer-ics2017 <tracer@njuics.org>
2023-04-13 23:03:33 +0800

    >  compile 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  23:03:33 up  2:05,  1 user,  load average: 0.49, 0.29, 0.14 8a33bc448e85252838a47f3d1061bbbd5f6eeb04

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -42,8 +42,8 @@ make_EHelper(sub) {
   rtl_sltu(&t1,&id_dest->val,&id_src->val);
   rtl_set_CF(&t1);
   rtl_xor(&t1,&id_dest->val,&id_src->val);
-  rtl_xor(&t2,&id_dest->val,&t0);
-  rtl_and(&t0,&t1,&t2);
+  rtl_xor(&t2,&id_dest->val,&t2);
+  rtl_and(&t0,&t0,&t1);
   rtl_msb(&t0,&t0,id_dest->width);
   rtl_set_OF(&t0);
 
