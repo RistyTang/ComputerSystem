11a188c4fafa548dc84079ec26417dc495f5f338
tracer-ics2017 <tracer@njuics.org>
2023-04-13 15:08:31 +0800

    >  compile 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  15:08:31 up  2:10,  1 user,  load average: 0.15, 0.13, 0.11 d30e156f88a639ed6bcee1801cc9735c55d3a159

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -45,7 +45,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
       break;
     case CC_NE:
       rtl_get_ZF(&t0);
-      rtl_eq0(dest,&t0);
+      rtl_eqi(dest,&t0,1);
       break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
