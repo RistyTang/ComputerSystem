4e3ee751c4219dc89706659e4a091687160c4b91
tracer-ics2017 <tracer@njuics.org>
2023-04-13 21:37:51 +0800

    >  compile 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  21:37:51 up 39 min,  1 user,  load average: 0.30, 0.57, 0.50 5cd2de232b3273aa6b69842c42b559c2e75aa7f4

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -84,7 +84,8 @@ make_EHelper(setcc) {
 
 make_EHelper(not) {
   //TODO();
-  rtl_not(&id_dest->val);
-  operand_write(id_dest,&id_dest->val);
+  rtl_mv(&t0,&id_dest->val);
+  rtl_not(&t0);
+  operand_write(id_dest,&t0);
   print_asm_template1(not);
 }
