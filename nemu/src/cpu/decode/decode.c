8cc399d071e8bd2a14175d9506c532160a863160
tracer-ics2017 <tracer@njuics.org>
2023-04-17 16:10:11 +0800

    >  compile 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  16:10:11 up 29 min,  1 user,  load average: 0.58, 0.30, 0.35 a83465d57ec2465f2d00536efccf975ff5f8a209

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -39,23 +39,23 @@ static inline make_DopHelper(SI) {
    op->simm = ???
    */
   //TODO();
+  /*
   if(op->width == 4)
   {
     op->simm = instr_fetch(eip,op -> width);
   }
-  /*
-  else if(op->width == 1)
-  {
-    op -> simm = (int8_t)op->simm;
-  }
-  */
   else
   {
     t0=(uint16_t )instr_fetch(eip,op -> width);
     rtl_sext(&t1,&t0,op->width);
     op->simm=t1;
   }
-  
+  */
+  op->simm = instr_fetch(eip,op -> width);
+  if(op->width == 1)
+  {
+    op -> simm = (int8_t)op -> simm;
+  }
 
   rtl_li(&op->val, op->simm);
 
