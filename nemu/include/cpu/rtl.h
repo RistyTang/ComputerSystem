5796272e8abfd291be147288085a5329a6b0e35b
tracer-ics2017 <tracer@njuics.org>
2023-04-17 15:33:53 +0800

    >  compile 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  15:33:53 up  1:08,  1 user,  load average: 1.52, 1.66, 1.28 1b28e678a5173de9a56b74ad234d190a69feb4c5

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -211,11 +211,6 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
   //TODO();
-  /*
-  rtl_andi(&t0,result,(0xffffffffu)>>(4-width)*8);
-  rtl_eq0(&t0,&t0);
-  rtl_set_ZF(&t0);
-  */
  if(*result == 0)
  {
    t0=1;
