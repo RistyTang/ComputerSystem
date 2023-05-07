f438a84b7aa89e6aac8369d59ee47b89af634908
tracer-ics2017 <tracer@njuics.org>
2023-05-07 16:11:58 +0800

    >  run 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  16:11:58 up  4:26,  1 user,  load average: 0.50, 0.45, 0.37 e2dc6cc12a98e06ec0293d679b60970263d68cf6

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,7 +11,7 @@ uintptr_t loader(_Protect *as, const char *filename)
   
   int fd = fs_open(filename,0,0);
   Log("filename = %s,fd = %d",filename,fd);
-  //fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
+  fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
   fs_close(fd);
   Log("file closed successfully\n");
   //ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
