23a5af27c4d83c07e9b5d0c52d9ac0a964e2c7e2
tracer-ics2017 <tracer@njuics.org>
2023-05-08 12:25:36 +0800

    >  run 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  12:25:36 up  1:54,  1 user,  load average: 0.48, 0.44, 0.44 855d6bd7a6ee1d31a8ef4426bef958e111854

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -136,7 +136,7 @@ static void get_display_info() {
     sscanf(buf, "%s", key);
     sscanf(delim + 1, "%s", value);
     printf("value:%d\n",value);
-    printf("key:%s",key);
+    printf("key:%s\n",key);
     if (strcmp(key, "WIDTH") == 0) sscanf(value, "%d", &screen_w);
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
   }
