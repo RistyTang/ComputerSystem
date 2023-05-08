5ee14deaf81acfab0dfb215c92bd66b0bcb8519a
tracer-ics2017 <tracer@njuics.org>
2023-05-08 11:41:42 +0800

    >  run 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  11:41:42 up  1:10,  1 user,  load average: 0.25, 0.34, 0.45 866d9cb6ade295f74e4266ffcaa6597d2ac8f66c

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -24,8 +24,8 @@ static Finfo file_table[] __attribute__((used)) = {
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern void ramdisk_write(const void* buf,off_t offset,size_t len);
 extern void fb_write(const void* buf,off_t offset,size_t len);
-extern size_t events_read(void *buf, size_t len);
 extern void dispinfo_read(void *buf, off_t offset, size_t len);
+extern size_t events_read(void *buf, size_t len);
 extern size_t get_ramdisk_size();
 
 void init_fs() {
@@ -117,38 +117,6 @@ int fs_close(int fd)
 ssize_t fs_write(int fd,void *buf,size_t len)
 {
   assert(fd >= 0 && fd < NR_FILES);
-  /*
-  //得到要操作的file指针
-  Finfo *fp = &file_table[fd];
-  ssize_t datalen = fp->size - fp->open_offset;
-  ssize_t writelen = datalen < len ? datalen : len;
-  //为1或者2时将buf首地址，len长度字节输出
-  if(fd == FD_STDOUT || fd == FD_STDERR)//1 2
-  {
-    char c;
-    for(int i=0;i<len;i++)
-    {
-      memcpy(&c,(void *)buf+i,1);
-      _putc(c);
-    }
-    return len;
-  }
-  else if(fd == FD_FB)//3
-  {
-    fb_write(buf,fp->open_offset,len);
-  }
-  else
-  {
-    if(fd < 6 || fd >=NR_FILES)
-    {
-      printf("wrong in sys_write for wrong fd\n");
-      return -1;
-    }
-    ramdisk_write(buf,fp->disk_offset + fp->open_offset,len);
-  }
-  fp->open_offset += writelen;
-  return writelen;
-  */
  /*
   if(fd < 3 || fd == FD_DISPINFO)
   {
@@ -177,17 +145,29 @@ ssize_t fs_write(int fd,void *buf,size_t len)
   file_table[fd].open_offset = cur_open_offset;
   return n;
   */
-  if(fd < 3)
-  {
-    Log("args invalid : fd <3\n");
-    return 0;
-  }
   int n = fs_filesz(fd) - file_table[fd].open_offset;
   if(n > len)
   {
     n = len;
   }
-  ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].open_offset,n);
+  char c;
+  switch (fd)
+  {
+  case FD_STDOUT://为1或者2时将buf首地址，len长度字节输出
+  case FD_STDERR:
+    for(int i=0;i<len;i++)
+    {
+      memcpy(&c,(void *)buf+i,1);
+      _putc(c);
+    }
+    return len;
+  case FD_FB://write to frame buffer
+    fb_write(buf,file_table[fd].open_offset,n);
+    break;
+  default:
+    ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].open_offset,n);
+    break;
+  }
   set_open_offset(fd,file_table[fd].open_offset + n);
   return n;
   
