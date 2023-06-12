9eaf63482e3c3f06e7559673e13f132615e03bb8
tracer-ics2017 <tracer@njuics.org>
2023-06-12 15:24:58 +0800

    >  compile 2013536 tang Linux tang-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  15:24:58 up 18:58,  1 user,  load average: 1.54, 0.65, 0.40 c02cd2e272d4a9fd5f6ab609723d915d6d1ee3b1

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -3,13 +3,34 @@
 #include <assert.h>
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+  //assert(0);
+  return (a * b) >> 16;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+  //assert(0);
+  //return 0;
+  FLOAT result = Fabs(a) / Fabs(b);
+  FLOAT m = Fabs(a);
+  FLOAT n = Fabs(b);
+  m = m % n;
+
+  for(int i = 0;i < 16;i++)
+  {
+    m <<= 1;
+    result <<= 1;
+    if(m >= n)
+    {
+      m -= n;
+      result ++;
+    }
+  }
+
+  if(((a ^ b) & 0x80000000) == 0x80000000)
+  {
+    result = -result;
+  }
+  return result;
 }
 
 FLOAT f2F(float a) {
@@ -23,13 +44,44 @@ FLOAT f2F(float a) {
    * performing arithmetic operations on it directly?
    */
 
-  assert(0);
-  return 0;
+  //assert(0);
+  union float_ {
+    struct 
+    {
+      uint32_t m : 23;
+      uint32_t e : 8;
+      uint32_t signal : 1;
+    };
+    uint32_t value;
+  };
+  union float_ f;
+  f.value = *((uint32_t*)(void*)&a);
+  int e = f.e - 127;
+
+  FLOAT result;
+  if(e <= 7)
+  {
+    result = (f.m | (1 << 23)) >> (7 - e);
+  }
+  else
+  {
+    result = (f.m | (1 << 23)) >> (e - 7);
+  }
+  if(f.signal == 0)
+  {
+    return result;
+  }
+  return (result | (1 << 31));
 }
 
 FLOAT Fabs(FLOAT a) {
-  assert(0);
-  return 0;
+  //assert(0);
+  //return 0;
+  if((a & 0x80000000) == 0)
+  {
+    return a;
+  }
+  return -a;
 }
 
 /* Functions below are already implemented */
